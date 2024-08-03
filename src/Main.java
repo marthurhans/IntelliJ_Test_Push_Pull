20e6e27458810db2a57837299e29d474b023f020
Mike Hans <marthurhans@gmail.com>
2024-08-03 10:53:34 -0400

    attempt to push from intellij

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Version 1 of tester");
+        System.out.println("2nd Test - pushing from IntelliJ");
+
+
+
+
     }
 }
+
