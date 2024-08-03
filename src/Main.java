ce8d0481e59652ff0bba9080d0866cafc5d7a411
Mike Hans <marthurhans@gmail.com>
2024-08-03 10:55:18 -0400

    checking if commit automatically adds changes from IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Version 1 of tester");
         System.out.println("2nd Test - pushing from IntelliJ");
+        System.out.println("3nd Test - committing and pushing from IntelliJ");
+
 
 
 
